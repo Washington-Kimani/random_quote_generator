ea0f0189fc2baa173c9a77343d3e3f4d05d06c4d
Washington Kimani <kimaniwashington39@gmail.com>
2023-11-02 11:49:13 +0300

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Random Quote Generator
+
+This react app uses axios to get data from an API serveing random quotes.
---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+  const [copied, setCopied] = useState(false);
+
+  const getQuote = () =>{
+    axios.get(`https://api.quotable.io/random`)
+    .then(res=>{
+      setAuthor(res.data.author);
+      setQuote(res.data.content);
+      setCopied(false)
+    }).catch(err=>{
+      console.error(err.message);
+    });
+  }
+
+
+  return (
+    <>
+      <div class="card">
+        <div className="quotes">
+          <p><small>Quote:</small><br /> {quote}</p>
+          <p><small>Author:</small><br /> {author}</p>
+        </div>
+        <div className="buttons">
+          <button class="glowing-btn" onClick={getQuote }><span class="glowing-txt">GET Q<span class="faulty-letter">U</span>OTE</span></button>
+          <CopyToClipboard text={`${quote} \nby: ${author}`} onCopy={()=>setCopied(true)}>
+            <button class="glowing-btn" ><span class="glowing-txt">C<span class="faulty-letter">O</span>PY QUOTE</span></button>
+          </CopyToClipboard>
+        </div>
+        { copied? <span className="copied">Copied!</span>: null}
+      </div>
+    </>
+  );
+}
+
+export default App;
